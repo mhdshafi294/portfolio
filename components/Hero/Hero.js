ca4eae67a3c21c1a270ab56fc424731cd31d7a84
Shafik AlNahhas <mhd.shafi2.n@gmail.com>
2023-05-01 22:42:38 +0300

    Hero section done

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -8,12 +8,14 @@ const Hero = (props) => (
   <Section row nopadding>
     <LeftSection>
       <SectionTitle main center>
-        Welcome To <br />
-        Shafik's Personal Portfolio
+        Hello there! <br />
+        This is <br />
+        Shafik's Portfolio
       </SectionTitle>
       <SectionText>
       The purpose of JavaScript Mastery is to help aspiring and established developers to take their development skills to the next level and build awesome apps.
       </SectionText>
+      <Button onClick={() => window.location = 'https://github.com/mhdshafi294'}>Learn More</Button>
     </LeftSection>
   </Section>
 );
