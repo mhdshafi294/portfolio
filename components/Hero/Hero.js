3a9a6d45fecda84d1164ff9122f4bb4bea04871c
Shafik AlNahhas <mhd.shafi2.n@gmail.com>
2023-05-01 22:13:48 +0300

    first commit

---
+++ components/Hero/Hero.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import Button from '../../styles/GlobalComponents/Button';
+import { LeftSection } from './HeroStyles';
+
+const Hero = (props) => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br />
+        Shafik's Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+      The purpose of JavaScript Mastery is to help aspiring and established developers to take their development skills to the next level and build awesome apps.
+      </SectionText>
+    </LeftSection>
+  </Section>
+);
+
+export default Hero;
