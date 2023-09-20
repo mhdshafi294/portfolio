8605c2c6312b66910d6d7dfbe2a0fb4df27b27b8
Shafik AlNahhas <mhd.shafi2.n@gmail.com>
2023-09-20 21:33:39 +0300

    add the hack effect

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 import {
   Section,
@@ -8,27 +8,113 @@ import {
 import Button from "../../styles/GlobalComponents/Button";
 import { LeftSection } from "./HeroStyles";
 
-const Hero = (props) => (
-  <Section row nopadding>
-    <LeftSection>
-      <SectionTitle main center>
-        Hello there! <br />
-        This is <br />
-        Shafik's Portfolio
-      </SectionTitle>
-      <SectionText>
-        I am an ambitious front-end React.JS, and Next13 JS developer, with a
-        particular interest in making things simple, robust, and pretty. I try
-        to keep up with best practices and am always looking for new things to
-        learn.
-      </SectionText>
-      <Button
-        onClick={() => (window.location = "https://github.com/mhdshafi294")}
-      >
-        Learn More
-      </Button>
-    </LeftSection>
-  </Section>
-);
+const Hero = (props) => {
+  const nameRef = useRef();
+  const helloRef = useRef();
+
+  const hackEffect = (event) => {
+    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const smallLetters = "abcdefghijklmnopqrstuvwxyz";
+    let interval = null;
+    let originalValue = event.target.innerText;
+    let iteration = 0;
+
+    clearInterval(interval);
+
+    interval = setInterval(() => {
+      event.target.innerText = event.target.innerText
+        .split("")
+        .map((letter, index) => {
+          if (index < iteration) {
+            return event.target.dataset.value[index];
+          }
+
+          return letters[Math.floor(Math.random() * 26)];
+        })
+        .join("");
+
+      if (iteration >= event.target.dataset.value.length) {
+        clearInterval(interval);
+      }
+
+      iteration += 1 / 3;
+    }, 30);
+  };
+
+  const hackEffectOnLoad = (text) => {
+    const smallLetters = "abcdefghijklmnopqrstuvwxyz";
+
+    let interval = null;
+    let originalValue = text.innerText;
+    let iteration = 0;
+
+    clearInterval(interval);
+
+    interval = setInterval(() => {
+      text.innerText = text.innerText
+        .split("")
+        .map((letter, index) => {
+          if (index < iteration) {
+            return text.dataset.value[index];
+          }
+
+          return smallLetters[Math.floor(Math.random() * 26)];
+        })
+        .join("");
+
+      if (iteration >= text.dataset.value.length) {
+        clearInterval(interval);
+      }
+
+      iteration += 1 / 3;
+    }, 30);
+  };
+
+  useEffect(() => {
+    console.log("hi");
+    hackEffectOnLoad(helloRef.current);
+  }, []);
+
+  return (
+    <Section row nopadding>
+      <LeftSection>
+        <SectionTitle main center>
+          <p
+            onMouseOver={(event) => {
+              hackEffect(event);
+            }}
+            ref={helloRef}
+            data-value="Hello there!"
+          >
+            Hello there! <br />
+          </p>
+          This is <br />
+          <span
+            onMouseOver={(event) => {
+              hackEffect(event);
+            }}
+            ref={nameRef}
+            data-value="Shafik's"
+          >
+            Shafik's
+          </span>{" "}
+          Portfolio!
+        </SectionTitle>
+        <br />
+        <SectionText>
+          I am an ambitious front-end React.JS, and Next13 JS developer, with a
+          particular interest in making things simple, robust, and pretty. I try
+          to keep up with best practices and am always looking for new things to
+          learn.
+        </SectionText>
+        <Button
+          onClick={() => (window.location = "https://github.com/mhdshafi294")}
+        >
+          Learn More
+        </Button>
+      </LeftSection>
+    </Section>
+  );
+};
 
 export default Hero;
