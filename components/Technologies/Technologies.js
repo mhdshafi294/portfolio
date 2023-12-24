3cb09eefb6d87970649f549455f175945094e3af
Shafik AlNahhas <mhd.shafi2.n@gmail.com>
2023-12-24 11:57:18 +0300

    update technologies

--- components/Technologies/Technologies.js
+++ components/Technologies/Technologies.js
@@ -1,9 +1,20 @@
-import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
-import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
+import React from "react";
+import { DiFirebase, DiReact, DiZend } from "react-icons/di";
+import {
+  Section,
+  SectionDivider,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import {
+  List,
+  ListContainer,
+  ListItem,
+  ListParagraph,
+  ListTitle,
+} from "./TechnologiesStyles";
 
-const Technologies = () =>  (
+const Technologies = () => (
   <Section id="tech">
     <SectionDivider divider />
     <SectionTitle>Technologies</SectionTitle>
@@ -17,8 +28,8 @@ const Technologies = () =>  (
         <ListContainer>
           <ListTitle>Front-End</ListTitle>
           <ListParagraph>
-          Experiece with <br />
-          React.js | Next.js
+            Experiece with <br />
+            React.JS | Next.JS
           </ListParagraph>
         </ListContainer>
       </ListItem>
@@ -27,8 +38,8 @@ const Technologies = () =>  (
         <ListContainer>
           <ListTitle>Back-End</ListTitle>
           <ListParagraph>
-          Little experiece with <br />
-          MongoDB
+            Next.JS API | Next.JS Auth <br />
+            Prisma DB | SQL & Mongo DB
           </ListParagraph>
         </ListContainer>
       </ListItem>
@@ -37,8 +48,8 @@ const Technologies = () =>  (
         <ListContainer>
           <ListTitle>UI/UX</ListTitle>
           <ListParagraph>
-          Experiece with <br />
-          tools like Figma
+            Experiece with <br />
+            tools like Figma
           </ListParagraph>
         </ListContainer>
       </ListItem>
